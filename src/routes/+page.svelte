3cda293d1aec079a6bf3f6742110c6a36b5da51d
Josef Zizka <josefzizka21@gmail.com>
2023-11-04 05:59:58 +0100

    fix: hot reload runs init()

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,9 @@
 	import NodeMenu from '../components/NodeMenu.svelte';
 	import EdgeMenu from '../components/EdgeMenu.svelte';
 	import LoadingIndicator from '../components/LoadingIndicator.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(init);
 </script>
 
 <CytoScapeEditor />
