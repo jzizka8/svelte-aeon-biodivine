9db1b390610f62e7d4856aae315578b763f4255a
Josef Zizka <josefzizka21@gmail.com>
2023-11-06 16:41:41 +0100

    feat: add hotkeys via npm, refactored quick help shortcut

--- src/components/QuickHelp.svelte
+++ src/components/QuickHelp.svelte
@@ -1,21 +1,15 @@
-<div id="quick-help">
-    <h3 style="display: block; margin: 0 auto; float: right;">HELP</h3>
-    <b>Network editor actions:</b>
-    <ul>
-        <li>Double click to create a new variable.</li>
-        <li>Drag from `+` to create a new regulation.</li>
-        <li>Select element (click) to show the context menu.</li>
-    </ul>
-    <b>Keyboard shortcuts:</b>
-    <ul>
-        <li>(⌫) Remove selected element.</li>
-        <li>(N) Add new variable.</li>
-        <li>(E) Edit name of the selected variable.</li>
-        <li>(F) Edit update function of the selected variable.</li>
-        <li>(O) Change observability of the selected regulation.</li>
-        <li>(M) Change monotonicity of the selected regulation.</li>
-        <li>(H) Show this help message.</li>
-    </ul>
-    <div style="width: 100%; text-align: center; font-size: 12px;">(this message disappears when you create the
-        first variable)</div>
+<script lang="ts">
+	import hotkeys from 'hotkeys-js';
+	import { onMount } from 'svelte';
+	let hidden = false;
+	//https://www.okupter.com/blog/sveltekit-document-is-not-defined
+	onMount(() => {
+		hotkeys('h', { keyup: true, keydown: true }, function (event, handler) {
+            event.preventDefault();
+            hidden = event.type === 'keyup';
+		});
+	});
+</script>
+
+<div id="quick-help" class="quick-help {hidden ? 'hidden' : ''}">
 </div>
