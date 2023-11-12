d827433f8f32684f1b1af65ec181e8b7bc390cc8
Josef Zizka <josefzizka21@gmail.com>
2023-11-12 15:50:09 +0100

    feat: menu reworked

--- src/components/SideMenu.svelte
+++ src/components/SideMenu.svelte
@@ -1,41 +1,83 @@
+<script>
+	import SideMenuItem from './SideMenuItem.svelte';
+</script>
+
 <div id="side-menu">
-    <div class="button-group-fake">
-        <img alt="Compute Bifurcation" id="side-menu-compute" src="img/play_circle_filled-48px.svg"
-            onclick="ComputeEngine.startComputation(LiveModel.exportAeon());" class="button-fake">
-        <span class="hint invisible">Start Analysis</span>
-    </div>
-
-    <div class="button-group">
-        <img alt="Engine" id="side-menu-engine" src="img/engine-48px.svg" class="button" tab-id="tab-engine">
-        <span id="engine-dot">●</span>
-        <span class="hint invisible">Compute Engine</span>
-    </div>
-
-    <div class="button-group">
-        <img alt="Import/Export" id="side-menu-import-export" src="img/file_copy-48px.svg" class="button"
-            tab-id="tab-import-export">
-        <span class="hint invisible">Import / Export</span>
-    </div>
-
-    <div class="button-group">
-        <img alt="Model" id="side-menu-model" src="img/model-48px.svg" class="button" tab-id="tab-model-editor">
-        <span class="hint invisible">Model Editor</span>
-    </div>
-
-    <div class="button-group-fake">
-        <img alt="Auto Layout" id="side-menu-layout" src="img/view_quilt-48px.svg"
-            onclick="CytoscapeEditor.layoutCose();" class="button-fake">
-        <span class="hint invisible">Apply Layout</span>
-    </div>
-
-    <div class="button-group">
-        <img alt="Model" id="side-menu-results" src="img/call_split-48px.svg" class="button"
-            tab-id="tab-results">
-        <span class="hint invisible">Results</span>
-    </div>
-
-    <div class="button-group">
-        <img alt="Help" id="side-menu-help" src="img/help-48px.svg" class="button" tab-id="tab-help">
-        <span class="hint invisible">About and Help</span>
-    </div>
+	<ul>
+		<button 
+			class="button  nav__item button--green "
+			title="You need to connect compute eninge in order to start analysis"
+		>
+			<img src="img/play_circle_filled-48px.svg" alt="" /> Start Analysis
+		</button>
+		<button class="button nav__item "><img src="img/view_quilt-48px.svg" alt="" /> Apply Layout</button>
+	</ul>
+	<nav>
+		<ul>
+			<li class="nav__item"><img src="img/engine-48px.svg" alt="" /> Compute Engine</li>
+			<li class="nav__item"><img src="img/file_copy-48px.svg" alt="" /> Import / Export</li>
+			<li class="nav__item"><img src="img/model-48px.svg" alt="" /> Model Editor</li>
+			<li class="nav__item"><img src="img/call_split-48px.svg" alt="" /> Results</li>
+			<li class="nav__item"><img src="img/help-48px.svg" alt="" /> About</li>
+		</ul>
+	</nav>
 </div>
+
+<style>
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		font-size: 1.25rem;
+		padding-left: 0;
+	}
+    nav{
+        margin-top: 6rem;
+    }
+	.nav__item {
+		padding: 1rem 0.75rem;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		border-radius: 1rem;
+	}
+	.nav__item:hover,
+	.nav__item.active {
+		background-color: var(--primary-color-light);
+        color: white;
+		cursor: pointer;
+	}
+	
+	.nav__item img {
+		width: 2rem;
+		height: 2rem;
+		margin-right: 0.5rem;
+	}
+
+    .button {
+        border:none;
+		background-color:var(--primary-color-light);
+		color: white;
+        font-size: 1.25rem;
+        padding-inline: 1rem;
+	}
+	.button:hover {
+		background-color:var(--primary-color);
+	}
+	.button.disabled {
+		background-color: gray;
+		color: black;
+		opacity: 0.5;
+		pointer-events: none;
+	}
+
+	.button img, .nav__item:hover img {
+		filter: invert(1);
+	}
+    .button--green{
+        background-color: var(--green-light) ;
+    }
+    .button--green:hover{
+        background-color: var(--green) ;
+    }
+</style>
