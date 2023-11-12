3825e8e25e02e5fc8ae58470ca924dec70ddd7fc
Josef Zizka <josefzizka21@gmail.com>
2023-11-13 00:54:51 +0100

    fix: formating

--- src/components/TabWrapper.svelte
+++ src/components/TabWrapper.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-    import type { SvelteComponent } from 'svelte';
-    import type { tabType } from '../types/types';
-    import HelpTab from './HelpTab.svelte';
-    import ImportExportTab from './ImportExportTab.svelte';
-    import ModelEditorTab from './ModelEditorTab.svelte';
+	import type { tabType } from '../types/types';
+	import HelpTab from './HelpTab.svelte';
+	import ImportExportTab from './ImportExportTab.svelte';
+	import ModelEditorTab from './ModelEditorTab.svelte';
 	import ResultsTab from './ResultsTab.svelte';
 	import EngingeTab from './EngingeTab.svelte';
 
-    const tabComponents = {
-        'about': HelpTab,
-        'import-export': ImportExportTab,
-        'model-editor': ModelEditorTab,
-        'results': ResultsTab,
-        'compute-engine': EngingeTab,
-    };
+	const tabComponents = {
+		about: HelpTab,
+		'import-export': ImportExportTab,
+		'model-editor': ModelEditorTab,
+		results: ResultsTab,
+		'compute-engine': EngingeTab
+	};
 
-    export let activeTab: tabType;
+	export let activeTab: tabType;
 
-    $: component =activeTab? tabComponents[activeTab]: null;
+	$: component = activeTab ? tabComponents[activeTab] : null;
 </script>
 
 {#if component}
