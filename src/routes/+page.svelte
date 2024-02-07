0ae2f66a29d4ec79df39960bb8ccceef623df567
Josef Zizka <josefzizka21@gmail.com>
2024-02-07 22:26:19 +0100

    chore: remove unused imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
-	import HelpTab from '../components/HelpTab.svelte';
 	import QuickHelp from '../components/QuickHelp.svelte';
 	import SideMenu from '../components/SideMenu.svelte';
-	import ResultsTab from '../components/ResultsTab.svelte';
 	import CytoScapeEditor from '../components/CytoScapeEditor.svelte';
-	import Version from '../components/Version.svelte';
 	import LogoType from '../components/LogoType.svelte';
-	import ImportExportTab from '../components/ImportExportTab.svelte';
-	import EngingeTab from '../components/EngingeTab.svelte';
-	import ModelEditorTab from '../components/ModelEditorTab.svelte';
 	import NodeMenu from '../components/NodeMenu.svelte';
 	import EdgeMenu from '../components/EdgeMenu.svelte';
 	import LoadingIndicator from '../components/LoadingIndicator.svelte';
-	import type { tabType } from '../types/types';
 	import TabWrapper from '../components/TabWrapper.svelte';
 
-	let activeTab: tabType = null;
 </script>
 
 <main>
