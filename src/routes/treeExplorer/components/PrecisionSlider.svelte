6fbc99bfdbaaae01f801104404a605ce327b167d
Josef Zizka <josefzizka21@gmail.com>
2024-02-18 00:23:35 +0100

    chore: extracted components

---
+++ src/routes/treeExplorer/components/PrecisionSlider.svelte
@@ -0,0 +1,14 @@
+
+
+<div id="precision">
+		<input
+			type="range"
+			min="5001"
+			max="10000"
+			value="10000"
+			class="seekbar"
+			id="precision-slider"
+		/>
+		Precision: <span id="precision-value">100%</span>
+	</div>
+
