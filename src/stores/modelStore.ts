a29a60f2d5c78af022b5c49646b004ec2b0bdaa1
Josef Zizka <josefzizka21@gmail.com>
2024-02-27 14:12:07 +0100

    fix: formating

--- src/stores/modelStore.ts
+++ src/stores/modelStore.ts
@@ -1,8 +1,7 @@
 import { writable } from 'svelte/store';
-import { type Model, type Variable, type Regulation, EdgeMonotonicity } from '../types/types'; // Adjust the import path to where your types are defined
+import type { Model, Variable, Regulation, EdgeMonotonicity } from '../types/types'; // Adjust the import path to where your types are defined
 import { generateRegulationId } from '$lib/utils/utils';
 
-// Initial state
 const initialState: Model = {
     id: '',
     name: 'Untitled Model',
@@ -11,10 +10,8 @@ const initialState: Model = {
     variables: []
 };
 
-// Create the writable store
 const modelStore = writable<Model>(initialState);
 
-// Store interaction functions
 const modelStoreActions = {
     updateModel: function (model: Model) {
         modelStore.set(model);
@@ -22,26 +19,35 @@ const modelStoreActions = {
     clearModel: function () {
         modelStore.set(initialState);
     },
-    addVariable: function (node: Variable) {
+    addVariable: function (variable: Variable) {
         modelStore.update((currentModel) => ({
-            ...currentModel, variables: [...currentModel.variables, node]
+            ...currentModel,
+            variables: [...currentModel.variables, variable]
         }));
     },
     setDescription: function (description: string) {
         modelStore.update((currentModel) => ({
-            ...currentModel, description
+            ...currentModel,
+            description
         }));
     },
     setName: function (name: string) {
         modelStore.update((currentModel) => ({
-            ...currentModel, name
+            ...currentModel,
+            name
         }));
     },
-    createRegulation: function (sourceId: string, targetId: string, monotonicity: EdgeMonotonicity.unspecified, observable: false) {
-
+    createRegulation: function (
+        sourceId: string,
+        targetId: string,
+        monotonicity: EdgeMonotonicity.unspecified,
+        observable: false
+    ) {
         modelStore.update((currentModel) => {
             const regulationId = generateRegulationId(sourceId, targetId);
-            const existingRegulation = currentModel.regulations.find(regulation => regulation.id === regulationId);
+            const existingRegulation = currentModel.regulations.find(
+                (regulation) => regulation.id === regulationId
+            );
             if (existingRegulation) {
                 return currentModel;
             }
@@ -58,48 +64,52 @@ const modelStoreActions = {
                 monotonicity,
                 observable
             };
-            return ({
-                ...currentModel, regulations: [...currentModel.regulations, newEdge]
-            })
+            return {
+                ...currentModel,
+                regulations: [...currentModel.regulations, newEdge]
+            };
         });
     },
 
-    addEdge: function (edge: Regulation) {
-        modelStore.update((currentModel) => ({
-            ...currentModel, regulations: [...currentModel.regulations, edge]
-        }));
-    },
     removeRegulation: function (edgeId: string) {
         modelStore.update((currentModel) => ({
-            ...currentModel, regulations: currentModel.regulations.filter((edge) => edge.id !== edgeId)
+            ...currentModel,
+            regulations: currentModel.regulations.filter((edge) => edge.id !== edgeId)
         }));
     },
     changeMonotonicity: function (edgeId: string, monotonicity: EdgeMonotonicity) {
         modelStore.update((currentModel) => ({
-            ...currentModel, regulations: currentModel.regulations.map((edge) => (edge.id === edgeId ? { ...edge, monotonicity } : edge))
+            ...currentModel,
+            regulations: currentModel.regulations.map((edge) =>
+                edge.id === edgeId ? { ...edge, monotonicity } : edge
+            )
         }));
     },
     removeVariable: function (nodeId: string) {
         modelStore.update((currentModel) => ({
-            ...currentModel, variables: currentModel.variables.filter((node) => node.id !== nodeId),
-            regulations: currentModel.regulations.filter((edge) => edge.source.id !== nodeId && edge.target.id !== nodeId)
+            ...currentModel,
+            variables: currentModel.variables.filter((node) => node.id !== nodeId),
+            regulations: currentModel.regulations.filter(
+                (edge) => edge.source.id !== nodeId && edge.target.id !== nodeId
+            )
         }));
     },
     renameVariable: function (nodeId: string, name: string) {
         modelStore.update((currentModel) => ({
-            ...currentModel, variables: currentModel.variables.map((node) => (node.id === nodeId ? { ...node, name } : node))
+            ...currentModel,
+            variables: currentModel.variables.map((node) =>
+                node.id === nodeId ? { ...node, name } : node
+            )
         }));
     },
     setVariableUpdateFunction: function (nodeId: string, updateFunction: string) {
-        modelStore.update(currentModel => {
-            const updatedVariables = currentModel.variables.map(node =>
+        modelStore.update((currentModel) => {
+            const updatedVariables = currentModel.variables.map((node) =>
                 node.id === nodeId ? { ...node, updateFunction } : node
             );
             return { ...currentModel, variables: updatedVariables };
         });
     }
-
 };
 
-// Export the store and the actions
 export { modelStore, modelStoreActions };
