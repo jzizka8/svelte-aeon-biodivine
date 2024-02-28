2ecf16326cf5efad817726f67a503a096c1d1fe6
Josef Zizka <josefzizka21@gmail.com>
2024-02-28 12:16:36 +0100

    feat: utils for calculating stats and formating

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,3 +1,33 @@
+import { EdgeMonotonicity, type Regulation } from "../../types/types";
+
 export function generateRegulationId(sourceId: string, targetId: string) {
-	return `${sourceId}_${targetId}`;
+    return `${sourceId}_${targetId}`;
+}
+
+export function regulationShortcut(regulation: Regulation) {
+    let arrow;
+    if (regulation.monotonicity === EdgeMonotonicity.activation) {
+        arrow = '->';
+    } else if (regulation.monotonicity === EdgeMonotonicity.inhibition) {
+        arrow = '-|';
+    }
+    else {
+        arrow = '-?';
+    }
+
+    if (regulation.observable) {
+        return arrow;
+    }
+    return `${arrow}?`;
+}
+
+export function nextMonotonicity(monotonicity: EdgeMonotonicity) {
+    switch (monotonicity) {
+        case EdgeMonotonicity.activation:
+            return EdgeMonotonicity.inhibition;
+        case EdgeMonotonicity.inhibition:
+            return EdgeMonotonicity.unspecified;
+        default:
+            return EdgeMonotonicity.activation;
+    }
 }
