f8efdc02ac6cb1b50c26d418d60c882c2a0d998f
Josef Zizka <josefzizka21@gmail.com>
2024-03-05 19:39:30 +0100

    feat: clear model button

--- src/components/SideMenu.svelte
+++ src/components/SideMenu.svelte
@@ -5,8 +5,10 @@
 	import { computeEngineStore } from '../stores/ComputeEngineStore';
 	import { liveModelStore } from '../stores/liveModelStore';
 	import CytoscapeEditor from '../script/CytoscapeEditor';
+	import { modelStore, modelStoreActions } from '../stores/modelStore';
+
 	let startAnalysisDisabled = false;
-	let applyLayoutDisabled = false;
+	$: modelEmpty = $modelStore.variables.length === 0;
 
 	$: ComputeEngine = $computeEngineStore;
 	$: LiveModel = $liveModelStore;
@@ -30,11 +32,22 @@
 		</li>
 		<li>
 			<button
-				class="button button--half-round button--primary {applyLayoutDisabled ? 'disabled' : ''}"
-				disabled={applyLayoutDisabled}
+				class="button button--half-round button--primary"
+				class:disabled={modelEmpty}
+				disabled={modelEmpty}
 				on:click={() => CytoscapeEditor.layoutCose()}
 			>
 				<img src="img/view_quilt-48px.svg" alt="" /> Apply Layout
+			</button>
+		</li>
+		<li>
+			<button
+				class="button button--half-round button--primary"
+				class:disabled={modelEmpty}
+				disabled={modelEmpty}
+				on:click={() => modelStoreActions.clearModel()}
+			>
+				<img src="img/delete-24px.svg" alt="" /> Clear Model
 			</button>
 		</li>
 	</ul>
