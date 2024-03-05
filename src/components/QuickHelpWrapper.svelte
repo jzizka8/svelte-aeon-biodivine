369693ba8cd028589a3b95b1556a52fa85b8c107
Josef Zizka <josefzizka21@gmail.com>
2024-03-05 19:31:24 +0100

    feat: hide quick help when model not empty

--- src/components/QuickHelpWrapper.svelte
+++ src/components/QuickHelpWrapper.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import hotkeys from 'hotkeys-js';
 	import { onMount } from 'svelte';
-	let hidden = false;
+	import { modelStore } from '../stores/modelStore';
+	$: hidden = $modelStore.variables.length !== 0;
+	
 	//https://www.okupter.com/blog/sveltekit-document-is-not-defined
 	onMount(() => {
 		hotkeys('h', { keyup: true, keydown: true }, function (event, handler) {
@@ -12,7 +14,7 @@
 </script>
 
 <!--  TODO: should also depent on model editor - show always if empty -->
-<div id="quick-help" class="quick-help {hidden ? 'hidden' : ''}">
+<div id="quick-help" class="quick-help" class:hidden>
 	<slot></slot>
 </div>
 
