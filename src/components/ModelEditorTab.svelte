3479a860518d90b8baf0324e77317a494adcc12f
Josef Zizka <josefzizka21@gmail.com>
2024-03-15 19:27:21 +0100

    temp: hide old stuff, but kept for init function

--- src/components/ModelEditorTab.svelte
+++ src/components/ModelEditorTab.svelte
@@ -4,7 +4,6 @@
 	import ModelVariable from './ModelVariable.svelte';
 	import type { ModelStatistics } from '../types/types';
 
-	import { liveModelStore } from '../stores/liveModelStore';
 	import { modelStore, modelStoreActions } from '../stores/modelStore';
 	import { calculateMaxDegrees } from '$lib/utils/modelStats';
 	import { nextMonotonicity } from '$lib/utils/utils';
@@ -53,7 +52,7 @@
 			type="text"
 			name="model-name"
 			placeholder="Untitled model"
-			style="font-size: 20px;"
+			style="font-size: 20px; display: none;"
 		/>
 		<input
 			id="model-name2"
@@ -72,60 +71,65 @@
 		id="model-description"
 		contenteditable
 		data-placeholder="(model description)"
-		style="margin-top: 4px; margin-bottom: 4px;"
+		style="display:none"
 	/>
 	<div class="invisible-input full-line" contenteditable>{@html $modelStore.description}</div>
 	<div style="height: 30px;">
 		<h3 style="font-family: 'FiraMono'; text-transform: uppercase;">● Overview</h3>
 	</div>
 	<ModelStats {modelStats} />
-	<div style="height: 40px;">
-		<h3 style="float: left; font-family: 'FiraMono'; text-transform: uppercase;">● Variables</h3>
-		<button
-			id="button-add-variable"
-			class="image-button"
-			on:click={() => $LiveModel.addVariable()}
-			style="float: right;">Add variable (N) <img src="img/add_box-24px.svg" /></button
-		>
-	</div>
-	<div style="clear: both;" />
-	<div id="model-variables" class="full-line" />
-	<div class="templates gone">
-		<div id="model-variable-template" class="model-variable">
-			<div class="invisible-input">
-				<input
-					class="variable-name"
-					type="text"
-					name="variable-name"
-					value="Variable name"
-					placeholder="(variable name)"
-					style="font-size: 18px;"
+
+	<button
+		id="button-add-variable"
+		class="image-button"
+		on:click={() => modelStoreActions.createVariable(undefined)}
+		style="float: right;">Add variable (N) <img src="img/add_box-24px.svg" /></button
+	>
+
+
+	<div class="old-code" style="display: none;">
+		<div style="height: 40px;">
+			<h3 style="float: left; font-family: 'FiraMono'; text-transform: uppercase;">● Variables</h3>
+		</div>
+		<div style="clear: both;" />
+		<div id="model-variables" class="full-line" />
+		<div class="templates gone">
+			<div id="model-variable-template" class="model-variable">
+				<div class="invisible-input">
+					<input
+						class="variable-name"
+						type="text"
+						name="variable-name"
+						value="Variable name"
+						placeholder="(variable name)"
+						style="font-size: 18px;"
+						spellcheck="false"
+						autocorrect="off"
+					/>
+				</div>
+				<img alt="Remove variable" src="img/delete-24px.svg" class="model-variable-remove button" />
+				<img alt="Show variable" src="img/search-24px.svg" class="model-variable-show button" />
+				<br />
+				<h4>● Regulators</h4>
+				<div class="model-variable-regulators full-line" />
+				<h4>● Update Function</h4>
+				<div
+					class="invisible-input full-line variable-function"
+					contenteditable
+					data-placeholder="(default)"
 					spellcheck="false"
 					autocorrect="off"
+					style="font-size: 16px; text-align: center;"
 				/>
+				<div class="variable-function-status" />
 			</div>
-			<img alt="Remove variable" src="img/delete-24px.svg" class="model-variable-remove button" />
-			<img alt="Show variable" src="img/search-24px.svg" class="model-variable-show button" />
-			<br />
-			<h4>● Regulators</h4>
-			<div class="model-variable-regulators full-line" />
-			<h4>● Update Function</h4>
-			<div
-				class="invisible-input full-line variable-function"
-				contenteditable
-				data-placeholder="(default)"
-				spellcheck="false"
-				autocorrect="off"
-				style="font-size: 16px; text-align: center;"
-			/>
-			<div class="variable-function-status" />
-		</div>
 
-		<div id="model-regulation-template" class="model-regulation">
-			<span class="model-regulation-regulator">Some name</span>
-			<span class="model-regulation-short">(->)</span>
-			<span class="model-regulation-observable">observable</span>
-			<span class="model-regulation-monotonicity">activation</span>
+			<div id="model-regulation-template" class="model-regulation">
+				<span class="model-regulation-regulator">Some name</span>
+				<span class="model-regulation-short">(->)</span>
+				<span class="model-regulation-observable">observable</span>
+				<span class="model-regulation-monotonicity">activation</span>
+			</div>
 		</div>
 	</div>
 	<h1>Svelte variables</h1>
