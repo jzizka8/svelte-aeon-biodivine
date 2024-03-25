690546c4d49dac77f1a8aa568813a4c474c5515a
Josef Zizka <josefzizka21@gmail.com>
2024-03-25 19:07:38 +0100

    chore: remove useless ehoptions

--- src/components/Cytoscape/config/edgeOptions.ts
+++ src/components/Cytoscape/config/edgeOptions.ts
@@ -2,26 +2,12 @@ import type { EdgeHandlesOptions } from "cytoscape-edgehandles";
 import { EdgeMonotonicity } from "../../../types/types";
 
 const edgeOptions: EdgeHandlesOptions = {
-    preview: true, // whether to show added edges preview before releasing selection
-    hoverDelay: 150, // time spent hovering over a target node before it is considered selected
     snap: false,
     snapThreshold: 50,
     snapFrequency: 15,
     noEdgeEventsInDraw: false,
     disableBrowserGestures: true,
-    nodeLoopOffset: -50,
-    // The `+` button should be drawn on top of each node
-    handlePosition: function (node) {
-        return 'middle top';
-    },
-    handleInDrawMode: false,
-    edgeType: function (sourceNode, targetNode) {
-        return 'flat';
-    },
-    // Loops are always allowed
-    loopAllowed: function (node) {
-        return true;
-    },
+   
     // Initialize edge with default parameters
     edgeParams: function (sourceNode, targetNode) {
         return { data: { observable: true, monotonicity: EdgeMonotonicity.unspecified } };
