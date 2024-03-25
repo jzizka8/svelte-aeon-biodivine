669fd01c25b33081c18c7ce9f3078941b918a5fc
Josef Zizka <josefzizka21@gmail.com>
2024-03-25 20:08:01 +0100

    feat: node menu keyboard shortcuts

--- src/components/NodeMenu.svelte
+++ src/components/NodeMenu.svelte
@@ -10,7 +10,15 @@
 	$: nodes = $selectedNodesStore?.items;
 	$: position = $selectedNodesStore?.position;
 	$: menuStyle =
-		nodes && position ? `position: absolute; top: ${position.y+15}px; left: ${position.x+120}px;` : '';
+		nodes && position
+			? `position: absolute; top: ${position.y + 15}px; left: ${position.x + 120}px;`
+			: '';
+
+	$: loopAllowed =
+		nodes?.length === 1 &&
+		!$modelStore.regulations.find(
+			(reg) => reg.source.id === nodes[0].id && reg.target.id === nodes[0].id
+		);
 
 	function handleRemove() {
 		if (nodes) {
@@ -34,7 +42,7 @@
 
 	function handleNewEdgeHandle() {
 		const cyNode = $cytoscapeStore?.$id(nodes[0].id);
-		if(cyNode && $edgehandlesStore){
+		if (cyNode && $edgehandlesStore) {
 			$edgehandlesStore.start(cyNode as any);
 		}
 	}
@@ -47,7 +55,28 @@
 		focusedInputStore.set('function');
 	}
 
-
+	onMount(() => {
+		hotkeys('e', { keyup: true, keydown: true }, function (event, handler) {
+			if (nodes.length != 1) return;
+			event.preventDefault();
+			handleNodeNameEdit();
+		});
+		hotkeys('f', { keyup: true, keydown: true }, function (event, handler) {
+			if (nodes.length != 1) return;
+			event.preventDefault();
+			handleNodeFunctionEdit();
+		});
+		hotkeys('backspace', { keyup: true, keydown: true }, function (event, handler) {
+			if (!nodes.length) return;
+			event.preventDefault();
+			handleRemove();
+		});
+		hotkeys('l', { keyup: true, keydown: true }, function (event, handler) {
+			if (!loopAllowed) return;
+			event.preventDefault();
+			handleLoopCreation();
+		});
+	});
 </script>
 
 <!-- A menu element that is shown for selected graph nodes in the editor. -->
@@ -56,7 +85,7 @@
 		{#if nodes.length === 1}
 			<button on:click={handleNodeNameEdit}>
 				<img alt="Edit name (E)" id="node-menu-edit-name" src="img/edit-24px.svg" />
-				Edit node
+				<!-- Edit node -->
 			</button>
 			<button on:click={handleNodeFunctionEdit}>
 				<img
@@ -67,23 +96,29 @@
 			</button>
 			<button on:mousedown={handleNewEdgeHandle}>
 				<img src="img/dot-arrow-up.svg" alt="" draggable="false" />
-				Add edge
+				<!-- Add edge -->
 			</button>
 			<!-- <button on:click={handleNewEdgeHandle}> edge from here </button> -->
+		{/if}
+		{#if loopAllowed}
+			<button on:click={handleLoopCreation}>
+				<img src="img/loop.svg" alt="" />
+				<!-- add Loop  -->
+			</button>
 		{/if}
 		{#if nodes.length === 2}
 			<button on:click={handleConnectionFrom}>
 				<img src="img/dot-arrow-up.svg" alt="" />
-				Connect from
+				<!-- Connect from -->
 			</button>
 			<button on:click={handleConnectionTo}
 				><img src="img/dot-arrow-down.svg" alt="" />
-				Connect here
+				<!-- Connect here -->
 			</button>
 		{/if}
 		<button on:click={handleRemove}>
 			<img alt="Remove (⌫)" id="node-menu-remove" src="img/delete-24px.svg" />
-			Remove
+			<!-- Remove -->
 		</button>
 	</div>
 </div>
