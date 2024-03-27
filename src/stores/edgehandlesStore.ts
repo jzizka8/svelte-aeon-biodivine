b1dc12c3e4d35dc87a6169a1a9d9730d27ee930a
Josef Zizka <josefzizka21@gmail.com>
2024-03-27 21:01:54 +0100

    remove: commented code

--- src/stores/edgehandlesStore.ts
+++ src/stores/edgehandlesStore.ts
@@ -1,9 +1,4 @@
 import type { EdgeHandlesInstance } from "cytoscape-edgehandles";
 import  { writable } from "svelte/store";
 
-// function createEdgehandlesStore() {
-//     return writable<EdgeHandlesInstance | null>(null); // Store holding cyInstance
-    
-//  }
-
 export const edgehandlesStore = writable<EdgeHandlesInstance | null>(null);
