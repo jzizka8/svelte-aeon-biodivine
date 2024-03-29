ed6fe079b4f74fc9c123b90b4027974e8d398786
Josef Zizka <josefzizka21@gmail.com>
2024-03-29 18:11:21 +0100

    feat: precision slider step

--- src/routes/treeExplorer/components/PrecisionSlider.svelte
+++ src/routes/treeExplorer/components/PrecisionSlider.svelte
@@ -1,4 +1,4 @@
 <div id="precision">
-	<input type="range" min="5001" max="10000" value="10000" class="seekbar" id="precision-slider" />
+	<input type="range" min="5025" max="10000" value="10000" step=25 class="seekbar" id="precision-slider" />
 	Precision: <span id="precision-value">100%</span>
 </div>
