495946f232c375b82ce45390b373585227419cbe
Josef Zizka <josefzizka21@gmail.com>
2024-03-29 12:27:30 +0100

    chore: main cytoscape more modular

---
+++ src/lib/types/treeExplorerTypes.ts
@@ -0,0 +1,44 @@
+export type Stability = {
+	variable: string;
+	data: {
+		vector: string[];
+		colors: number;
+	}[];
+};
+export type CardinalityClass = {
+	cardinality: number;
+	class: string;
+	fraction: number;
+};
+
+export type DecisionAttribute = {
+	id: number;
+	name: string;
+	left: CardinalityClass[];
+	right: CardinalityClass[];
+	gain: number;
+	leftTotal: number;
+	rightTotal: number;
+};
+export enum NodeType {
+	Unprocessed = 'unprocessed',
+	Decision = 'decision',
+	Leaf = 'leaf'
+}
+
+export type Class = {
+	cardinality: number;
+	class: string;
+};
+
+export type TreeNode = {
+	type: NodeType;
+	cardinality: number;
+	classes: Class[];
+	id: number;
+	attribute_id?: number;
+	left?: number;
+	right?: number;
+	attribute_name?: string;
+	all_classes?: Class[];
+};
---
+++ src/routes/components/CytoscapeEditor.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Graph, GraphNode, GraphEdge, NodeMenu, EdgeMenu } from '$lib/cytoscape';
+	import { modelStore } from '$lib/stores/modelStore';
+
+	$: nodes = $modelStore.variables.map((variable) => ({
+		id: variable.id,
+		label: variable.name,
+		initPosition: variable.initPosition
+	}));
+
+	$: edges = $modelStore.regulations.map((regulation) => ({
+		id: regulation.id,
+		source: regulation.source.id,
+		target: regulation.target.id,
+		monotonicity: regulation.monotonicity,
+		observable: regulation.observable
+	}));
+</script>
+
+<NodeMenu />
+<EdgeMenu />
+<Graph>
+	{#each nodes as node (node.id)}
+		<GraphNode {node} />
+	{/each}
+
+	{#each edges as edge (edge.id)}
+		<GraphEdge {edge} />
+	{/each}
+</Graph>
