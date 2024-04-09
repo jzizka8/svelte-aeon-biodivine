bc6406f280fcff1aed2bb9ddea17b7cabc81b986
Josef Zizka <josefzizka21@gmail.com>
2024-04-09 21:00:21 +0200

    fix: stability analysis btn remove from index

--- src/routes/treeExplorer/components/index.ts
+++ src/routes/treeExplorer/components/index.ts
@@ -3,4 +3,3 @@ export { default as MixedTab } from './MixedTab.svelte';
 export { default as QuickHelp } from './QuickHelp.svelte';
 export { default as LeafTab } from './LeafTab.svelte';
 export { default as DecisionTab } from './DecisionTab.svelte';
-export { default as StabilityAnalysisButton } from './StabilityAnalysisButton.svelte';
