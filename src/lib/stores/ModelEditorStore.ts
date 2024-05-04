419e45e0cff74ef3247420ff7e0816d100e20edd
Josef Zizka <josefzizka21@gmail.com>
2024-05-04 13:44:55 +0200

    remove: old store

--- src/lib/stores/ModelEditorStore.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import ModelEditor from '../../script/ModelEditor';
-
-// Create a read-only store for the ModelEditor module
-export const modelEditorStore = writable(ModelEditor, () => {});
