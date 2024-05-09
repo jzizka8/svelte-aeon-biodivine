b5957eb6c9a09fdac7e1ffed08081bb83039feb2
Samuel Pastva <sam.pastva@gmail.com>
2024-05-09 14:58:24 +0200

    Demo of background computation plus decision tree creation.

---
+++ src/compute_worker.js
@@ -0,0 +1,20 @@
+import init, { Conversions, ComputationResult } from 'aeon-wasm';
+
+console.log('Worker is starting...');
+
+onmessage = async (e) => {
+	await init();
+
+	const model_string = e.data['model'];
+	try {
+		const result = ComputationResult.compute(model_string, (partial_result) => {
+			postMessage({ type: 'progress', data: partial_result });
+		});
+		postMessage({ type: 'result', data: result.get_results(), tree_data: result.get_tree_data() });
+	} catch (e) {
+		console.log('Computation failed', e);
+		postMessage({ type: 'error', error: e });
+	}
+};
+
+console.log('Worker initialized.');
