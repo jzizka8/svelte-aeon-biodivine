27f4062d10db2f1df05ec48932e7f609bd75eb60
Josef Zizka <josefzizka21@gmail.com>
2024-05-12 22:45:16 +0200

    feat: temp solution to build wasm for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasmPack from 'vite-plugin-wasm-pack';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [sveltekit(), wasmPack(['./aeon-wasm'])],
+	plugins: [
+		sveltekit(),
+		wasmPack(['./aeon-wasm']),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'aeon-wasm/pkg/aeon_wasm_bg.wasm',
+					dest: '_app/immutable/workers/assets'
+				}
+			]
+		})
+	],
 	server: {
 		fs: {
 			// Allow serving files from one level up to the project root
