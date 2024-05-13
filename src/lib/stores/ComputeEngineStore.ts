24e2398a86b196b82a49101f1db94447db8b74e3
Josef Zizka <josefzizka21@gmail.com>
2024-05-13 19:58:55 +0200

    remove: compute engine and messages

--- src/lib/stores/ComputeEngineStore.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import { ComputeEngine } from '../../script/ComputeEngine';
-
-// Create the ComputeEngine store using the writable function
-export const computeEngineStore = writable(ComputeEngine, () => {});
